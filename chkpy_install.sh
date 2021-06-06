dfaffb623d016555bdfb7bb9b9928478b46e5c31
test <test@localhost.localdomain>
2021-06-06 16:26:21 -0400

    changed python version detection for instaling pip, currently re-installs pip at run time, needs to have a check added so that if pip exists it doesnt re-install it. sent more outputs to dev/null in favor of my own outputs

--- chkpy_install.sh
+++ chkpy_install.sh
@@ -11,55 +11,109 @@ echo " "
 echo "welcome to the chkpy installer. please make a selection"
 echo "install chkpy (1) or remove chkpy (2) or remove witout dependances(3)"
 read input
+PipVersion=none
+###############################################################################
 
+
+###############################################################################
+#handles installation of autopep8, pylint, pycodestyle, and pip if it applys
 if [ $input -eq 1 ]
 then
 echo " welcome to chkpy!"
 echo "installing dependances: pycodestyle,pylint & autopep8"
 echo " "
-currentver="$(python3 --version)"
-echo "checking python version for compatibilty, minimum version is ${currentver}!"
-echo " "
-requiredver="3.8.5"
-if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ];
+
+#checks pythoin version so later code can grab the right pip version
+PythonVersion="$( python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')"
+
+#if python version 3.0.0 or greater (expressed as 300) it grabs the most
+#current version of pip3 and installs it
+if [ $PythonVersion -ge 30 ]
 then
-echo "Minimum or greater version requirement met!"
-else
-echo "you do not have the minimum required python version you need ${requiredver} or better" || die
-fi
-echo " "
-echo "##################################################################"
-echo " "
-pip3 install pycodestyle
-echo " "
-pip3 install pylint
-echo " "
-pip3 install autopep8
-chmod +wrx chkpy
-cp chkpy /bin
-echo " "
-echo "##################################################################"
-echo " "
+PipVersion=pip3
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py > /dev/null
+python3 get-pip.py --user
+/usr/bin/python3 -m pip install --upgrade pip > /dev/null
 fi
 
+#if python version 2.7.0 or lesser (expressed as 270) it grabs the most
+#current version of pip2 and installs it, althoug depricated.
+if [ $PythonVersion -le 27 ]
+then
+PipVersion=pip2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py > /dev/null
+python2 get-pip.py --user
+/usr/bin/python2 -m pip install --upgrade pip > /dev/null
+fi
+
+#installing pylint, pycodestyle, and autopep8.
+echo " "
+echo "##################################################################"
+echo " "
+echo "installing pycodestyle"
+
+$PipVersion install pycodestyle > /dev/null
+echo " "
+echo "installing pylint"
+$PipVersion install pylint > /dev/null
+echo " "
+echo "installing autopep8"
+$PipVersion install autopep8 > /dev/null
+sudo chmod +wrx chkpy
+echo ""
+echo "installing chkpy"
+sudo cp chkpy /bin
+echo " "
+echo "##################################################################"
+echo " "
+fi
+###############################################################################
+
+
+###############################################################################
+#handles removeing chkpy, pycodestyle, pylint and autopep8
 if [ $input -eq 2 ]
 then
+#checks pythoin version so later code can grab the right pip version
+PythonVersion="$( python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')"
+if [ $PythonVersion -ge 30 ]
+then
+PipVersion=pip3
+fi
+
+if [ $PythonVersion -le 27 ]
+then
+PipVersion=pip2
+fi
+
 echo "sad to see you go"
 echo "removeing chkpy and dependances..."
 echo " "
 echo "##################################################################"
 echo " "
-pip3 uninstall pycodestyle -y
+echo "uninstalling pycodestyle"
 echo " "
-pip3 uninstall pylint -y
+$PipVersion uninstall pycodestyle -y
 echo " "
-pip3 uninstall autopep8 -y
-rm /bin/chkpy
+echo "uninstalling pylint"
+echo " "
+$PipVersion uninstall pylint -y
+echo " "
+echo "uninstalling autopep8"
+echo " "
+$PipVersion uninstall autopep8 -y
+echo " "
+sudo rm /bin/chkpy
+echo "removeing chkpy"
 echo " "
 echo "##################################################################"
 echo " "
 fi
+###############################################################################
 
+
+###############################################################################
+#handles removeing chkpy but leaves pycodestyle, pylint and autopep8 behind
 if [ $input -eq 3 ]
 then
 echo "sad to see you go...keep the tools ok?"
@@ -67,7 +121,7 @@ echo "removeing chkpy"
 echo " "
 echo "##################################################################"
 echo " "
-rm /bin/chkpy
+sudo rm /bin/chkpy
 echo "removed chkpy"
 echo " "
 echo "##################################################################"
